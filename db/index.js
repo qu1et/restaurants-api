d7f5ef56e47558618e2f05f68e220ab1b4558a87
Gasan Alkadarskiy <alkadarskiy.off@gmail.com>
2020-11-23 20:02:54 +0300

    Update db connection

--- db/index.js
+++ db/index.js
@@ -3,12 +3,15 @@ const { Pool } = require('pg');
 
 const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
-const proConfig = process.env.PG_URL;
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: process.env.NODE === 'production'
                         ? proConfig
-                        : devConfig
+                        : devConfig,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = {
